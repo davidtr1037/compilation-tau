d70407c083c5538712e4683ee7a182796a57b85f
Yarden Ben Bassat <yarden.benbassat@imperva.com>
2021-11-21 17:48:19 +0200

    This is a commit only for 'example' sakes, and not yet ready or complete. Added derivation rules to stmt and needed ast's for them

---
+++ src/ex2/src/AST/AST_STMT_RETURN_EXP.java
@@ -0,0 +1,55 @@
+package AST;
+
+public class AST_STMT_RETURN_EXP extends AST_STMT
+{
+
+    public AST_EXP exp;
+
+    /*******************/
+    /*  CONSTRUCTOR(S) */
+    /*******************/
+    public AST_STMT_ASSIGN()
+    {
+        /******************************/
+        /* SET A UNIQUE SERIAL NUMBER */
+        /******************************/
+        SerialNumber = AST_Node_Serial_Number.getFresh();
+
+        /***************************************/
+        /* PRINT CORRESPONDING DERIVATION RULE */
+        /***************************************/
+        System.out.print("====================== stmt -> RETURN EXP\n");
+
+        this.exp = exp;
+
+    }
+
+    /*********************************************************/
+    /* The printing message for an assign statement AST node */
+    /*********************************************************/
+    public void PrintMe()
+    {
+        /********************************************/
+        /* AST NODE TYPE = AST ASSIGNMENT STATEMENT */
+        /********************************************/
+        System.out.print("AST NODE RETURN EXP STMT\n");
+
+        /******************************/
+        /* RECURSIVELY PRINT EXP ... */
+        /*****************************/
+        if (exp != null) exp.PrintMe();
+
+        /***************************************/
+        /* PRINT Node to AST GRAPHVIZ DOT file */
+        /***************************************/
+        AST_GRAPHVIZ.getInstance().logNode(
+            SerialNumber,
+            "RETURN EXP\n");
+
+        /****************************************/
+        /* PRINT Edges to AST GRAPHVIZ DOT file */
+        /****************************************/
+        AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,exp.SerialNumber);
+
+    }
+}
---
+++ src/ex2/src/AST/AST_STMT_RETURN_NULL.java
@@ -0,0 +1,40 @@
+package AST;
+
+public class AST_STMT_RETURN_NULL extends AST_STMT
+{
+
+    /*******************/
+    /*  CONSTRUCTOR(S) */
+    /*******************/
+    public AST_STMT_ASSIGN()
+    {
+        /******************************/
+        /* SET A UNIQUE SERIAL NUMBER */
+        /******************************/
+        SerialNumber = AST_Node_Serial_Number.getFresh();
+
+        /***************************************/
+        /* PRINT CORRESPONDING DERIVATION RULE */
+        /***************************************/
+        System.out.print("====================== stmt -> RETURN \n");
+
+    }
+
+    /*********************************************************/
+    /* The printing message for an assign statement AST node */
+    /*********************************************************/
+    public void PrintMe()
+    {
+        /********************************************/
+        /* AST NODE TYPE = AST ASSIGNMENT STATEMENT */
+        /********************************************/
+        System.out.print("AST NODE RETURN NULL STMT\n");
+
+        /***************************************/
+        /* PRINT Node to AST GRAPHVIZ DOT file */
+        /***************************************/
+        AST_GRAPHVIZ.getInstance().logNode(
+            SerialNumber,
+            "RETURN\n");
+    }
+}
